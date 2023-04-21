9065721fdf0ec0f7543836ca9c82cdca6817ed92
SbsCruz <sebas.cruz750@gmail.com>
2023-04-21 16:58:26 -0500

    add: JS script

---
+++ program.js
@@ -0,0 +1,5 @@
+// Pedimos datos mediante un prompt y los guardamos
+// en la variable nombreUsuario
+let nombreUsuario = prompt("Escribe tu nombre:");
+
+alert("Hola como estás " + nombreUsuario + "?");
